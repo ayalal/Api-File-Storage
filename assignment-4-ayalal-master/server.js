92015112c5f9c3b056d4f986f2de7b0311cb1c43
Lorenzo Ayala <Lorenzo.Ayala@a-dec.com>
2020-05-27 09:55:10 -0700

    docker-compose now works

---
+++ assignment-4-ayalal-master/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const multer = require('multer');
+const morgan = require('morgan');
+
+const api = require('./api');
+const { connectToDB } = require('./lib/mongo');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const imageTypes = {
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const upload = multer({
+  // dest: `${__dirname}/uploads`
+  storage: multer.diskStorage({
+    destination: `${__dirname}/uploads`,
+    filename: (req, file, callback) => {
+      const filename = crypto.pseudoRandomBytes(16).toString('hex');
+      const extension = imageTypes[file.mimetype];
+      callback(null, `${filename}.${extension}`);
+    }
+  }),
+  fileFilter: (req, file, callback) => {
+    callback(null, !!imageTypes[file.mimetype]);
+  }
+});
+
+/*
+ * Morgan is a popular logger.
+ */
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+/*
+ * All routes for the API are written in modules in the api/ directory.  The
+ * top-level router lives in api/index.js.  That's what we include here, and
+ * it provides all of the routes.
+ */
+app.use('/', api);
+
+app.use('*', function (req, res, next) {
+  res.status(404).json({
+    error: "Requested resource " + req.originalUrl + " does not exist"
+  });
+});
+
+connectToDB(() => {
+  app.listen(port, () => {
+    console.log("== Server is running on port", port);
+  });
+});
